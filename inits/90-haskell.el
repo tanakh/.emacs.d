419de68248bd406970c9f6aafd168d1134437b7c
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2014-04-05 04:45:38 +0900

    disable shm

--- inits/90-haskell.el
+++ inits/90-haskell.el
@@ -19,7 +19,7 @@
           (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)))
 
 ;; structured-haskell-mode
-(require 'shm)
-(add-hook 'haskell-mode-hook 'structured-haskell-mode)
-(set-face-background 'shm-current-face "#eee8d5")
-(set-face-background 'shm-quarantine-face "lemonchiffon")
+;(require 'shm)
+;(add-hook 'haskell-mode-hook 'structured-haskell-mode)
+;(set-face-background 'shm-current-face "#eee8d5")
+;(set-face-background 'shm-quarantine-face "lemonchiffon")
